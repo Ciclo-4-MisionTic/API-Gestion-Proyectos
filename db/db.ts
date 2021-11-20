8d0f3fbe795facea5d6899f0979fb01e85d94b76
Sofia Botero <sdboteropa@unal.edu.co>
2021-11-20 00:32:32 -0500

    realizando cambios en rama sofia

---
+++ db/db.ts
@@ -0,0 +1,12 @@
+import { connect } from "mongoose";
+
+const conectarBD = async () =>{
+    return await connect(
+        'mongodb+srv://admin:AdminCodex@gestorcodexproject.7gfui.mongodb.net/GestorCodexProject?retryWrites=true&w=majority'
+    ).then(() =>{
+        console.log('Conexión exitosa');
+    }).catch((e)=>{
+        console.error("Error conectando a la bd", e);
+    })
+    };
+    export default conectarBD;
