f173d65e9ab275a45c1f152e67d9e594d7f246bd
Juan Jose Montoya <janjoentrepreneur@gmail.com>
2021-12-05 16:19:04 -0500

    editar,crear,elminar observaciones

--- models/avances/resolvers.js
+++ models/avances/resolvers.js
@@ -24,8 +24,60 @@ const resolversAvance = {
             })
 
 
-            return avanceCreado;
+            return 
+            
+            avanceCreado;
         },
+        editaravance: async (parent,args)=>{
+            const avanceEditado = await avanceModel.findByIdAndUpdate(args._id,{...args.campos},{new:true})
+            return avanceEditado;
+        },
+
+        eliminaravance: async(parent, args) =>{
+            if(Object.keys(args).includes("_id")){
+                const avanceEliminado = await avanceModel.findOneAndDelete({_id: args._id});
+                return avanceEliminado;   
+            } else if(Object.keys(args).includes("nombre")){
+                const avanceEliminado = await avanceModel.findOneAndDelete({ nombre: args.nombre});
+                return avanceEliminado;   
+            }
+        },
+
+        crearobservaciones: async (parent,args)=>{
+            const proyectoConobservaciones= await Model.findByIdAndUpdate(args.idProyecto,{
+                $addToSet:{
+                    observaciones:{... args.campos},
+                },
+            }, {new:true});
+
+            return proyectoConObjetivos
+        },
+
+        editarobservaciones: async (parent, args) =>{
+            const proyectoobservacionesEditado = await ProjectModel.findByIdAndUpdate(args.idProyecto,
+                {
+                $set:{
+                    [`objetivos.${args.indexobservaciones}.descripcion`]: args.campos.descripcion,
+                    [`objetivos.${args.indexobservaciones}.tipo`]: args.campos.tipo
+                }
+            },{new:true});
+            return proyectoobservacionesEditado;
+        },
+
+        eliminarObjetivo: async (parent, args) => {
+            const proyectoobservacionesEliminado = await ProjectModel.findByIdAndUpdate(
+              { _id: args.idProyecto },
+              {
+                $pull: {
+                  observaciones: {
+                    _id: args.idobservaciones,
+                  },
+                },
+              },
+              { new: true }
+            );
+            return proyectoobservacionesEliminado;
+            }
     },
 };
 
--- models/avances/tipos.js
+++ models/avances/tipos.js
@@ -21,6 +21,26 @@ const tiposAvance = gql`
             creadoPor: String!
         ):Avance
     }
+    type Mutation {
+        crearobservacion(
+            nombre: String!
+            presupuesto: Float!
+            fechaInicio: Date!
+            fechaFin: Date!
+            estado:  Enum_EstadoProyecto!
+            fase: Enum_FaseProyecto!
+            lider: String!
+            observacion: [crearobservacion]
+        ): Proyecto
+        
+        editarProyecto(_id: String!,campos: camposProyecto!): Proyecto
+        eliminarProyecto(_id:String nombre: String): Proyecto
+        crearobservacion(idProyecto:String!, campos: camposObjetivo): Proyecto
+        
+        editarobservacion(idProyecto:String!, indexobservacion:Int!, campos:camposObjetivo): Proyecto
+        
+        eliminarobservacion(idProyecto: String!, idobservacion: String!): Proyecto
+        }
 `;
 
 export {tiposAvance}
