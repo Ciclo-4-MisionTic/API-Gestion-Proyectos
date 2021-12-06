af510c35a5ee3f7ee36f0248e7e9395c2442d782
eyvargash29 <eyvargash@unal.edu.co>
2021-12-05 19:39:48 -0500

    Errores

--- models/avance/tipos.js
+++ models/avance/tipos.js
@@ -1,40 +1,18 @@
 import {gql} from 'apollo-server-express';
 
 const tiposAvance = gql`
-
-    type Observacion{
-        _id: ID
-        descripcion:String!
-    }
-
-    input crearObservacion{
-        _id: ID
-        descripcion:String!
-    }
-    input camposObservacion{
-        descripcion:String!,
-    }
-
-    input camposAvance {
-        _id: ID
-        fecha: Date
-        descripcion: String
-
-    } 
-
     type Avance{
         _id: ID!
         fecha: Date!
         descripcion: String!
+        observaciones: [String]
         proyecto: Proyecto!
         creadoPor: Usuario!
-        observaciones: [Observacion]
     }
-
 
     type Query{
         Avances: [Avance]
-        filtrarAvance(idAvance: String!): [Avance]
+        filtrarAvance(idProyecto: String!): [Avance]
     }
 
     type Mutation{
@@ -43,13 +21,16 @@ const tiposAvance = gql`
             descripcion: String!
             proyecto: String!
             creadoPor: String!
-            observaciones: [crearObservacion]
         ):Avance
 
-        crearObservacion(idAvance:String!, campos: camposObservacion): Avance
+        editarAvance(
+            _id: String!
+            descripcion: String,
+        ): Avance
 
-        
+        eliminarAvance(_id:String descripcion: String): Avance
     }
+
 `;
 
 export {tiposAvance}
